dbcbe41a259f10dd8ff055edc0ca938b9efe2d53
hainiu <hainiu@example.com>
2023-08-14 21:55:48 +0800

    第一次提交

---
+++ src/main/java/com/hainiu/Ddd.java
@@ -0,0 +1,10 @@
+package com.hainiu;
+
+public class Ddd {
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        demo1.say("maven");
+        Demo2 demo2 = new Demo2();
+        demo2.syHello();
+    }
+}
