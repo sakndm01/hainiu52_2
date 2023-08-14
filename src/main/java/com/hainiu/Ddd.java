e108000d0b973b886ec81c5ffadfce5cf81b4233
hainiu <hainiu@example.com>
2023-08-14 22:20:46 +0800

    Revert "第3次提交"
    This reverts commit 344a6e38

--- src/main/java/com/hainiu/Ddd.java
+++ src/main/java/com/hainiu/Ddd.java
@@ -8,6 +8,7 @@ public class Ddd {
         demo2.syHello();
         System.out.println(1111);
         System.out.println(1111);
-        System.out.println(4444);
+        System.out.println(3333);
+        System.out.println(3333);
     }
 }
